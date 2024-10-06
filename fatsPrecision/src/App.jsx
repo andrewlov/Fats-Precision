587446349b109a50f9c12488927dacc4ae6fbeeb
andrewlov <aklov@ucdavis.edu>
2024-10-06 04:35:06 -0700

    Added titles

--- fatsPrecision/src/App.jsx
+++ fatsPrecision/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 import './index.css'
 import Services from './Components/Services/Services.jsx'
+import Title from './Components/Title/Title.jsx'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer"/>
         <Services/>
       </div>
       
